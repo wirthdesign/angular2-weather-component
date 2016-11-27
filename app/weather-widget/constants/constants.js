622518692112faea366151b73fa33d9c6523c9fa
Kevin Wirth <me@wirthdesign.com>
2016-11-27 13:39:35 -0500

    Lecture 61

--- app/weather-widget/constants/constants.js
+++ app/weather-widget/constants/constants.js
@@ -1,4 +1,6 @@
 "use strict";
 exports.FORECAST_KEY = "3dddbd81d19ff41633d8365b8563ed38";
 exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
+exports.GOOGLE_MAP_KEY = "AIzaSyBEif7K-SVGGsNC4lrY1PEBcLzeJ55kTO0";
+exports.GOOGLE_MAP_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
 //# sourceMappingURL=constants.js.map
--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import { FORECAST_KEY, FORECAST_ROOT } from '../constants/constants';
+import { FORECAST_KEY, FORECAST_ROOT, GOOGLE_MAP_KEY, GOOGLE_MAP_ROOT } from '../constants/constants';
 
 @Injectable()
 
