c5379d407ff54614c3f75ae4d968ba64d473508c
Kevin Wirth <me@wirthdesign.com>
2016-11-27 13:44:21 -0500

    Removing API keys

--- app/weather-widget/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-export const FORECAST_KEY = "3dddbd81d19ff41633d8365b8563ed38";
-export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
-
-export const GOOGLE_MAP_KEY = "AIzaSyBEif7K-SVGGsNC4lrY1PEBcLzeJ55kTO0";
-export const GOOGLE_MAP_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
